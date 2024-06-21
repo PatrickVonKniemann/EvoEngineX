f8d5ed91a546742100f613bcd27244c8adfbc640
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-06-21 13:30:27 +0200

    Add routing

---
+++ DomainModels/Links.cs
@@ -0,0 +1,9 @@
+namespace DomainModels;
+
+public static class Links
+{
+    public static string Codebases = "codebases";
+    public static string CloudProfile = "cloud-profile";
+    public static string UserProfile = "user-profile";
+    public static string BuildRuns = "build-runs";
+}
