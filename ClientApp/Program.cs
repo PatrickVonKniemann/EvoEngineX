eb7f6e6e47ad8ffc3687b610b30efc19d2687b8d
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-06-27 14:32:23 +0200

    - changed to blazrozi def

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -14,38 +14,68 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 
 // Background
-const string primaryColor = "#131313"; // Dark
-const string secondaryColor = "#1C1A1C"; // Light dark
-const string darkColor = "#000000"; //pure black
-const string infoColor = "#BB86FD"; // light purple
+const string primaryBackgroundColor = "#131313"; // Dark
+const string secondaryBackgroundColor = "#1C1A1C"; // Light dark
+const string darkBackgroundColor = "#000000"; //pure black
+const string infoBackgroundColor = "#BB86FD"; // light purple
 
 // Buttons color
-const string primaryButtonColor = "#A45DE8"; // purple
-const string primaryButtonAccentColor = "#7F39FB"; // purple darker
-const string secondaryButtonColor = "#03DAC5"; // teal
+const string purple = "#A45DE8"; // purple
+const string purpleAccentDark = "#7F39FB"; // purple darker
+const string purpleAccentDarkWithOpacity = "#503FCD7F"; // purple darker
+const string teal = "#03DAC5"; // teal
+
+// Text color
+const string primaryTextColor = "#E1E1E1";
+const string secondaryTextColor = "#666666";
+const string infoTextColor = "#A77AE0";
+const string successTextColor = "#6200ED";
+
 
 var theme = new Theme
 {
+    // IsGradient = true,
+    // IsRounded = true,
     ColorOptions = new ThemeColorOptions
     {
-        Primary = primaryButtonColor,
-        Secondary = secondaryButtonColor,
-        Dark = primaryButtonAccentColor,
+        Primary = purple,
+        Secondary = teal,
+        Dark = purpleAccentDark,
+        Success = purpleAccentDarkWithOpacity
     },
     BackgroundOptions = new ThemeBackgroundOptions
     {
-        Primary = primaryColor,
-        Secondary = secondaryColor,
-        Dark = darkColor,
-        Info = infoColor,
+        Primary = primaryBackgroundColor,
+        Secondary = secondaryBackgroundColor,
+        Dark = darkBackgroundColor,
+        Info = infoBackgroundColor,
     },
     TextColorOptions = new ThemeTextColorOptions
     {
-        Primary = "#E1E1E1",
-        Secondary = "#666666",
-        Info = "#A77AE0",
-        Success = "#6200ED",
-    }
+        Primary = primaryTextColor,
+        Secondary = secondaryTextColor,
+        Info = infoTextColor,
+        Success = successTextColor
+    },
+    BarOptions = new ThemeBarOptions
+    {
+        DarkColors = new ThemeBarColorOptions
+        {
+            BackgroundColor = darkBackgroundColor,
+            Color = secondaryTextColor,
+            ItemColorOptions = new ThemeBarItemColorOptions
+            {
+                ActiveBackgroundColor = secondaryBackgroundColor,
+                HoverBackgroundColor = secondaryBackgroundColor,
+                ActiveColor = primaryTextColor,
+                HoverColor = primaryTextColor,
+            },
+            BrandColorOptions = new ThemeBarBrandColorOptions
+            {
+                BackgroundColor = darkBackgroundColor
+            }
+        }
+    },
 };
 
 builder.Services.AddSingleton(theme);
