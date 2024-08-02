0bdec5bad317d55d4f75e48a883ba23e2111762a
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-08-02 11:54:05 +0200

    - add helpers

--- ClientApp/Services/CodeBaseService.cs
+++ ClientApp/Services/CodeBaseService.cs
@@ -2,4 +2,4 @@ using ExternalDomainEntities.CodeBaseDto.Query;
 
 namespace ClientApp.Services;
 
-public class CodeBaseService(HttpClient httpClient) : GenericService<ReadCodeBaseListResponse, ReadCodeBaseResponse, ReadCodeBaseListRequest>(httpClient, "http://localhost:5002/code-base");
+public class CodeBaseService(HttpClient httpClient) : GenericService<ReadCodeBaseListByUserIdResponse, ReadCodeBaseResponse, ReadCodeBaseListRequest>(httpClient, "http://localhost:5002/code-base");
--- Helpers/HttpRequestHelper.cs
+++ Helpers/HttpRequestHelper.cs
@@ -1,23 +1,25 @@
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text;
+using System.Web;
 using Newtonsoft.Json;
 
 namespace Helpers;
 
 public static class HttpRequestHelper
 {
-    public static HttpRequestMessage CreateGetRequestWithBody(string url, object body)
+    public static HttpRequestMessage CreateGetRequestWithQuery(string baseUrl, string relativeUrl, Dictionary<string, string> queryParams)
     {
-        var content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
-
-        var request = new HttpRequestMessage
+        var uriBuilder = new UriBuilder(new Uri(new Uri(baseUrl), relativeUrl));
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        foreach (var param in queryParams)
         {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri(url, UriKind.Relative),
-            Content = content
-        };
-        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        
-        return request;
+            query[param.Key] = param.Value;
+        }
+        uriBuilder.Query = query.ToString();
+    
+        return new HttpRequestMessage(HttpMethod.Get, uriBuilder.Uri);
     }
+
+
 }
