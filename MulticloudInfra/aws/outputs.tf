d2cdbe3cec6c2c00cbcd4394fcd76f8084034f5a
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-09-18 12:48:30 +0200

    wip

---
+++ MulticloudInfra/aws/outputs.tf
@@ -0,0 +1,3 @@
+output "aws_ecr_repository_url" {
+  value = aws_ecr_repository.my_dotnet_api.repository_url
+}
---
+++ MulticloudInfra/azure/outputs.tf
@@ -0,0 +1,3 @@
+output "azure_acr_login_server" {
+  value = azurerm_container_registry.my_dotnet_acr.login_server
+}
