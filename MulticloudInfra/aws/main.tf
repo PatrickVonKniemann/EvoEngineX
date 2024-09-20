6cf78a73f2207a2ad996604095a3a399afaa27fb
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-09-20 11:51:04 +0200

    Violets are blue

--- MulticloudInfra/aws/main.tf
+++ MulticloudInfra/aws/main.tf
@@ -3,13 +3,16 @@ provider "aws" {
 }
 
 # Create an ECS cluster
-resource "aws_ecs_cluster" "test_api3_cluster" {
-  name = "test-api3-cluster"
+resource "aws_ecs_cluster" "test_dotnet_api_test_cluster" {
+  name = "test-dotnet-api-test-cluster"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Create an IAM Role for ECS task execution
-resource "aws_iam_role" "TaskExecutionEcs3" {
-  name = "TaskExecutionEcs3"
+resource "aws_iam_role" "TaskExecutionDotnetApiTest" {
+  name = "TaskExecutionDotnetApiTest"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -21,25 +24,33 @@ resource "aws_iam_role" "TaskExecutionEcs3" {
       }
     }]
   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Attach the required Amazon ECS Task Execution Role policy
-resource "aws_iam_role_policy_attachment" "TaskExecutionEcs3Policy" {
-  role       = aws_iam_role.TaskExecutionEcs3.name
+resource "aws_iam_role_policy_attachment" "TaskExecutionDotnetApiTestPolicy" {
+  role       = aws_iam_role.TaskExecutionDotnetApiTest.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "test_api3_task" {
-  family                   = "test-api3"
+resource "aws_ecs_task_definition" "test_dotnet_api_test_task" {
+  family                   = "test-dotnet-api-test"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.TaskExecutionEcs3.arn
+  execution_role_arn       = aws_iam_role.TaskExecutionDotnetApiTest.arn
   memory                   = "512"
   cpu                      = "256"
 
   container_definitions = jsonencode([{
-    name      = "test-api3"
+    name      = "test-dotnet-api-test"
     image     = "${var.ecr_repository_url}:latest"
     essential = true
     portMappings = [{
@@ -50,19 +61,23 @@ resource "aws_ecs_task_definition" "test_api3_task" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        "awslogs-group"         = "/ecs/test-api3"
+        "awslogs-group"         = "/ecs/test-dotnet-api-test"
         "awslogs-region"        = "us-east-1"
         "awslogs-stream-prefix" = "ecs"
       }
     }
   }])
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # ECS Service
-resource "aws_ecs_service" "test_api3_service" {
-  name            = "test-api3-service"
-  cluster         = aws_ecs_cluster.test_api3_cluster.id
-  task_definition = aws_ecs_task_definition.test_api3_task.arn
+resource "aws_ecs_service" "test_dotnet_api_test_service" {
+  name            = "test-dotnet-api-test-service"
+  cluster         = aws_ecs_cluster.test_dotnet_api_test_cluster.id
+  task_definition = aws_ecs_task_definition.test_dotnet_api_test_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
@@ -73,40 +88,56 @@ resource "aws_ecs_service" "test_api3_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.test_api3_target_group.arn
-    container_name   = "test-api3"
+    target_group_arn = aws_lb_target_group.test_dotnet_api_test_target_group.arn
+    container_name   = "test-dotnet-api-test"
     container_port   = 80
   }
 
-  depends_on = [aws_lb_listener.test_api3_listener]
+  depends_on = [aws_lb_listener.test_dotnet_api_test_listener]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Application Load Balancer
-resource "aws_lb" "test_api3_lb" {
-  name               = "test-api3-lb"
+resource "aws_lb" "test_dotnet_api_test_lb" {
+  name               = "test-dotnet-api-test-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["sg-097a6a7e63727eb39"]
   subnets            = ["subnet-0a9f456c125753831", "subnet-075e54dfa8cf31286"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Target Group for the Load Balancer
-resource "aws_lb_target_group" "test_api3_target_group" {
-  name        = "test-api3-tg"
+resource "aws_lb_target_group" "test_dotnet_api_test_target_group" {
+  name        = "test-dotnet-api-test-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = "vpc-0b40a65925c8d2210"
   target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Listener for the Load Balancer
-resource "aws_lb_listener" "test_api3_listener" {
-  load_balancer_arn = aws_lb.test_api3_lb.arn
+resource "aws_lb_listener" "test_dotnet_api_test_listener" {
+  load_balancer_arn = aws_lb.test_dotnet_api_test_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.test_api3_target_group.arn
+    target_group_arn = aws_lb_target_group.test_dotnet_api_test_target_group.arn
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
