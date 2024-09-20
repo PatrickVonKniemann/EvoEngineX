ba0b860163c828664ccecab5a209ee93a25eccd5
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-09-20 13:22:41 +0200

    gs

--- MulticloudInfra/aws/main.tf
+++ MulticloudInfra/aws/main.tf
@@ -1,3 +1,34 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ecr_repository_url" {
+  description = "The name of the ECR repository"
+  type        = string
+  default     = "376129885232.dkr.ecr.us-east-1.amazonaws.com/test-api"  # You can set a default value or leave it blank for dynamic input
+}
+
+# Variables definition
+variable "ECS_CLUSTER_NAME" {
+  type = string
+}
+
+variable "ECR_REPOSITORY_NAME" {
+  type = string
+}
+
+variable "IAM_ROLE_NAME" {
+  type = string
+}
+
+variable "LOAD_BALANCER_NAME" {
+  type = string
+}
+
+variable "TARGET_GROUP_NAME" {
+  type = string
+}
 
 # AWS provider configuration
 provider "aws" {
