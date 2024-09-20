e877733f860ff7d9d9f0962b639f938103d32ce8
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-09-20 11:21:53 +0200

    Roses are red

--- MulticloudInfra/aws/main.tf
+++ MulticloudInfra/aws/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 # Create an ECS cluster
-resource "aws_ecs_cluster" "test_api2_cluster" {
-  name = "test-api2-cluster"
+resource "aws_ecs_cluster" "test_api3_cluster" {
+  name = "test-api3-cluster"
 }
 
 # Create an IAM Role for ECS task execution
-resource "aws_iam_role" "TaskExecutionEcs" {
-  name = "TaskExecutionEcs"
+resource "aws_iam_role" "TaskExecutionEcs3" {
+  name = "TaskExecutionEcs3"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -24,22 +24,22 @@ resource "aws_iam_role" "TaskExecutionEcs" {
 }
 
 # Attach the required Amazon ECS Task Execution Role policy
-resource "aws_iam_role_policy_attachment" "TaskExecutionEcsPolicy" {
-  role       = aws_iam_role.TaskExecutionEcs.name
+resource "aws_iam_role_policy_attachment" "TaskExecutionEcs3Policy" {
+  role       = aws_iam_role.TaskExecutionEcs3.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "test_api2_task" {
-  family                   = "test-api2"
+resource "aws_ecs_task_definition" "test_api3_task" {
+  family                   = "test-api3"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.TaskExecutionEcs.arn
+  execution_role_arn       = aws_iam_role.TaskExecutionEcs3.arn
   memory                   = "512"
   cpu                      = "256"
 
   container_definitions = jsonencode([{
-    name      = "test-api2"
+    name      = "test-api3"
     image     = "${var.ecr_repository_url}:latest"
     essential = true
     portMappings = [{
@@ -50,7 +50,7 @@ resource "aws_ecs_task_definition" "test_api2_task" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        "awslogs-group"         = "/ecs/test-api2"
+        "awslogs-group"         = "/ecs/test-api3"
         "awslogs-region"        = "us-east-1"
         "awslogs-stream-prefix" = "ecs"
       }
@@ -59,10 +59,10 @@ resource "aws_ecs_task_definition" "test_api2_task" {
 }
 
 # ECS Service
-resource "aws_ecs_service" "test_api2_service" {
-  name            = "test-api2-service"
-  cluster         = aws_ecs_cluster.test_api2_cluster.id
-  task_definition = aws_ecs_task_definition.test_api2_task.arn
+resource "aws_ecs_service" "test_api3_service" {
+  name            = "test-api3-service"
+  cluster         = aws_ecs_cluster.test_api3_cluster.id
+  task_definition = aws_ecs_task_definition.test_api3_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
@@ -73,17 +73,17 @@ resource "aws_ecs_service" "test_api2_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.test_api2_target_group.arn
-    container_name   = "test-api2"
+    target_group_arn = aws_lb_target_group.test_api3_target_group.arn
+    container_name   = "test-api3"
     container_port   = 80
   }
 
-  depends_on = [aws_lb_listener.test_api2_listener]
+  depends_on = [aws_lb_listener.test_api3_listener]
 }
 
 # Application Load Balancer
-resource "aws_lb" "test_api2_lb" {
-  name               = "test-api2-lb"
+resource "aws_lb" "test_api3_lb" {
+  name               = "test-api3-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["sg-097a6a7e63727eb39"]
@@ -91,8 +91,8 @@ resource "aws_lb" "test_api2_lb" {
 }
 
 # Target Group for the Load Balancer
-resource "aws_lb_target_group" "test_api2_target_group" {
-  name        = "test-api2-tg"
+resource "aws_lb_target_group" "test_api3_target_group" {
+  name        = "test-api3-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = "vpc-0b40a65925c8d2210"
@@ -100,13 +100,13 @@ resource "aws_lb_target_group" "test_api2_target_group" {
 }
 
 # Listener for the Load Balancer
-resource "aws_lb_listener" "test_api2_listener" {
-  load_balancer_arn = aws_lb.test_api2_lb.arn
+resource "aws_lb_listener" "test_api3_listener" {
+  load_balancer_arn = aws_lb.test_api3_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.test_api2_target_group.arn
+    target_group_arn = aws_lb_target_group.test_api3_target_group.arn
   }
 }
