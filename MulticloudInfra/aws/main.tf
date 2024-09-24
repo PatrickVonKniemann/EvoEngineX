125259586acd5fe74d06aebc78c60f693fe9c931
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-09-24 14:47:31 +0200

    gd

--- MulticloudInfra/aws/main.tf
+++ MulticloudInfra/aws/main.tf
@@ -43,7 +43,7 @@ resource "aws_ecs_task_definition" "simpledotnetapi_task" {
   container_definitions = jsonencode([
     {
       name      = "simpledotnetapi"
-      image     = "376129885232.dkr.ecr.us-east-1.amazonaws.com/simpledotnetapi:v1.1.95"  # Explicitly specify the manual address and tag
+      image     = "376129885232.dkr.ecr.us-east-1.amazonaws.com/simpledotnetapi:latest"  # Explicitly specify the manual address and tag
       essential = true
       portMappings = [{
         containerPort = 8080
