3ea24b1d6d57b30e0ba908bc04f70b238d7055f6
PatrickVonKniemann <patrik.kolenovsky@gmail.com>
2024-09-24 14:08:27 +0200

    gege

--- InfrastructureTestCode/Program.cs
+++ InfrastructureTestCode/Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 // var version = Environment.GetEnvironmentVariable("APP_VERSION") ?? "1.0.0";
 // app.MapGet("/", () => $"Hello World! version is {version}");
-app.MapGet("/", () => $"Hello World! version is 1.0.0");
+app.MapGet("/appversion", () => $"Hello World! version is 1.0.0");
 
 app.Run();
